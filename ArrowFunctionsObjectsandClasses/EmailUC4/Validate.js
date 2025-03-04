164bc0016b74cb3dd28672c1770dd912a71af4e1
Bhupendra shakya <bhupendrashakya714@gmail.com>
2025-03-04 13:20:03 +0530

    [Bhupendra Shakya] Add. a program to validate the email with two mandatory fields .

---
+++ ArrowFunctionsObjectsandClasses/EmailUC4/Validate.js
@@ -0,0 +1,18 @@
+const validateEmail = email => /^abc(\.[a-zA-Z0-9_\+\-\.\']+)?@bridgelabz\.co(\.[a-z]+)?$/.test(email);
+
+// Valid test cases:
+console.log(validateEmail("abc@bridgelabz.co"));          
+console.log(validateEmail("abc.xyz@bridgelabz.co"));      
+console.log(validateEmail("abc_123@bridgelabz.co"));       
+console.log(validateEmail("abc+xyz@bridgelabz.co"));       
+console.log(validateEmail("abc-xyz@bridgelabz.co"));       
+console.log(validateEmail("abc.xyz@bridgelabz.co.in"));   
+console.log(validateEmail("abc_123+xyz@bridgelabz.co.us")); 
+// Invalid test cases:
+console.log(validateEmail("abc@bridgelabz.com"));        
+console.log(validateEmail("abc.xyz@bridge.co"));          
+console.log(validateEmail("abcd@bridgelabz.co"));         
+console.log(validateEmail("abc.xyz@bridgelabz"));         
+console.log(validateEmail("abc@bridgelabz"));              
+console.log(validateEmail("abc.#xyz@bridgelabz.co"));     
+console.log(validateEmail("abc.xyz@bridgelabz.co.in!")); 
